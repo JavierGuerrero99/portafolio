a6fc50786d030c2e59deccbaff91693a1f8fd96e
Javier Guerrero <javieradrianrgue@ufps.edu.co>
2025-09-20 14:11:07 -0500

    Se corrigen errores en el package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+// import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,13 +9,13 @@ export default defineConfig({
 plugins: [
   react(),
   tailwindcss(),
-  viteStaticCopy({
-    targets: [
-      {
-        src: 'node_modules/pdfjs-dist/build/pdf.worker.min.js',
-        dest: ''
-      }
-    ]
-  })
+  // viteStaticCopy({
+  //   targets: [
+  //     {
+  //       src: 'node_modules/pdfjs-dist/build/pdf.worker.min.js',
+  //       dest: ''
+  //     }
+  //   ]
+  // })
 ],
 })
